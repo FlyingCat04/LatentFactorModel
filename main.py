bb245bfa723bbafee41cf586c97a0f227dac31f6
FlyingCat2703 <hanlyngoc1204@gmail.com>
2026-02-05 17:54:30 +0700

    update env

--- main.py
+++ main.py
@@ -195,4 +195,4 @@ async def trigger_training(req: TrainRequest):
     }
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host=settings.APP_HOST, port=settings.APP_PORT, reload=True)
